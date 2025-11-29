3f5ba02a90472800faec3301b0303158765da3e1
UnknownGod2011 <tanushshah2006@gmail.com>
2025-11-29 11:46:47 +0530

    feat(ux): Add scroll-to-top on route change for better navigation UX

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext.tsx';
+import ScrollToTop from './components/ScrollToTop.tsx';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <CartProvider>
         <App />
       </CartProvider>
